6fce6157bf97c0adf720557ba121deff27e72551
CObitimah <obitimahcephas@gmail.com>
2023-11-01 17:55:05 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - sum of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: (0)
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
