24fb3ed998fa8b9a43a2c66f87367125684c0315
CObitimah <obitimahcephas@gmail.com>
2023-11-18 16:40:35 -0800

    It is not what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - fuction that prints new line
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (argc);
+}
