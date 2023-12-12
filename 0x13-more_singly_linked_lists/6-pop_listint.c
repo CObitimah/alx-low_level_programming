c035511ff06d579e8c2b1fa6cb3ed9620037aeeb
CObitimah <obitimahcephas@gmail.com>
2023-12-12 05:28:56 -0800

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,14 +10,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
+	int n;
 
-	if (head == NULL)
-		return (NULL);
-	while (*head)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-	}
-	return (tmp);
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	tmp = *head;
+	*head = tmp->next;
+	n = tmp->n;
+	free(tmp);
+	return (n);
 }
