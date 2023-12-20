55376d040d01ac3ec4d9f456e2c000d1854fcec9
CObitimah <obitimahcephas@gmail.com>
2023-12-20 00:14:21 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,4 +13,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+int file_a;
+int vletters;
+int rwr;
+if (!filename)
+	return (-1);
+file_a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+if (file_a == -1)
+	return (-1);
+
+if (!text_content)
+	text_content = "";
+
+for (vletters = 0; text_content[vletters]; vletters++)
+	;
+rwr = write(file_a, text_content, vletters);
+if (rwr == -1)
+	return (-1);
+close(file_a);
+return (1);
+}
