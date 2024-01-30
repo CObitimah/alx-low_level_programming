8616ab1399d46554044f340c551196781e3f6da2
CObitimah <obitimahcephas@gmail.com>
2024-01-30 06:54:54 +0000

    Lets call C functions from Python

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mod - modify two integers
+ * @a: first int
+ * @b: second int
+ * Return: answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mul - multiplicate two integers
+ * @a: first int
+ * @b: second int
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
