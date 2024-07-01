cd945064ac09bafe82e0621ed7efdc2a8c116a63
CObitimah <obitimahcephas@gmail.com>
2024-07-01 04:15:32 -0400

    Binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,8 +7,8 @@
 
 
 int linear_search(int *array, size_t size, int value);
-/*int binary_search(int *array, size_t size, int value);*/
-int binary_search(int *array, size_t left, size_t right, int value);
+int binary_search(int *array, size_t size, int value);
+/*int binary_search(int *array, size_t left, size_t right, int value);*/
 int exponential_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 
